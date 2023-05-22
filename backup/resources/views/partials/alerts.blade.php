b8dc1528fa6292e88eddf53c3751f05e327b4bdd
Shakeel Ahmad <shakeel2717@gmail.com>
2023-05-23 03:54:23 +0500

    Remove Bckup, update composer

--- backup/resources/views/partials/alerts.blade.php
+++
@@ -1,6 +0,0 @@
-@if ($alertFm = Session::get('success'))
-    <div class="alert alert-success alert-block">
-        <button type="button" class="close" data-dismiss="alert">×</button>
-        <strong>{{ $alertFm }}</strong>
-    </div>
-@endif
